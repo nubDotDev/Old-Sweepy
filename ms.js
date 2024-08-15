6d92a27ad37539cd91d8010a3ea5d6ef81aefcca
nubDotDev <nubdotdev@gmail.com>
2024-08-14 23:57:40 -0700

    v1.3.0

--- ms.js
+++ ms.js
@@ -934,6 +934,26 @@ class Website {
         }
     }
 
+    getDims() {
+        let width = 0;
+        for (let x = 0; ; x++) {
+            const elem = this.getCell(x, 0);
+            if (!elem || elem.style.display === "none") {
+                break;
+            }
+            width++;
+        }
+        let height = 0;
+        for (let y = 0; ; y++) {
+            const elem = this.getCell(0, y);
+            if (!elem || elem.style.display === "none") {
+                break;
+            }
+            height++;
+        }
+        return [width, height];
+    }
+
     getDiv() {
         return this.consented ? div : probabilitiesDiv;
     }
@@ -947,10 +967,6 @@ class Website {
     }
 
     inject() {
-        throw new Error("Not implemented");
-    }
-
-    getDims() {
         throw new Error("Not implemented");
     }
 
@@ -1010,25 +1026,25 @@ class MinesweeperOnlineCom extends Website {
         rightColumn.insertBefore(this.getDiv(), rightColumn.firstChild);
     }
 
-    getDims() {
-        let width = 0;
-        for (let i = 1; ; i++) {
-            const elem = document.getElementById("1_" + i);
-            if (!elem || elem.style.display === "none") {
-                break;
-            }
-            width++;
-        }
-        let height = 0;
-        for (let i = 1; ; i++) {
-            const elem = document.getElementById(i + "_1");
-            if (!elem || elem.style.display === "none") {
-                break;
-            }
-            height++;
-        }
-        return [width, height];
-    }
+    // getDims() {
+    //     let width = 0;
+    //     for (let i = 1; ; i++) {
+    //         const elem = document.getElementById("1_" + i);
+    //         if (!elem || elem.style.display === "none") {
+    //             break;
+    //         }
+    //         width++;
+    //     }
+    //     let height = 0;
+    //     for (let i = 1; ; i++) {
+    //         const elem = document.getElementById(i + "_1");
+    //         if (!elem || elem.style.display === "none") {
+    //             break;
+    //         }
+    //         height++;
+    //     }
+    //     return [width, height];
+    // }
 
     getCell(x, y) {
         return document.getElementById(y + 1 + "_" + (x + 1));
@@ -1043,25 +1059,22 @@ class MinesweeperOnlineCom extends Website {
     }
 
     getMineCount() {
-        return (
-            100 *
-                parseInt(
-                    document.getElementById("mines_hundreds").className[4]
-                ) +
-            10 * parseInt(document.getElementById("mines_tens").className[4]) +
-            parseInt(document.getElementById("mines_ones").className[4])
-        );
+        const hundreds = document.getElementById("mines_hundreds").className[4];
+        const tens = document.getElementById("mines_tens").className[4];
+        const ones = document.getElementById("mines_ones").className[4];
+        if (hundreds === "-") {
+            return -(10 * parseInt(tens) + parseInt(ones));
+        }
+        return 100 * parseInt(hundreds) + 10 * parseInt(tens) + parseInt(ones);
     }
 
     getState() {
-        if (document.getElementsByClassName("facesmile").length) {
-            return IN_PROGRESS;
-        } else if (document.getElementsByClassName("facedead").length) {
+        if (document.getElementsByClassName("facedead").length) {
             return LOST;
         } else if (document.getElementsByClassName("facewin").length) {
             return WON;
         }
-        throw new Error("Unknown state");
+        return IN_PROGRESS;
     }
 }
 
@@ -1108,25 +1121,25 @@ class MinesweeperOnline extends Website {
         );
     }
 
-    getDims() {
-        let width = 0;
-        for (let i = 0; ; i++) {
-            const elem = document.getElementById("cell_" + i + "_0");
-            if (!elem) {
-                break;
-            }
-            width++;
-        }
-        let height = 0;
-        for (let i = 0; ; i++) {
-            const elem = document.getElementById("cell_0_" + i);
-            if (!elem) {
-                break;
-            }
-            height++;
-        }
-        return [width, height];
-    }
+    // getDims() {
+    //     let width = 0;
+    //     for (let i = 0; ; i++) {
+    //         const elem = document.getElementById("cell_" + i + "_0");
+    //         if (!elem) {
+    //             break;
+    //         }
+    //         width++;
+    //     }
+    //     let height = 0;
+    //     for (let i = 0; ; i++) {
+    //         const elem = document.getElementById("cell_0_" + i);
+    //         if (!elem) {
+    //             break;
+    //         }
+    //         height++;
+    //     }
+    //     return [width, height];
+    // }
 
     getCell(x, y) {
         return document.getElementById("cell_" + x + "_" + y);
@@ -1165,12 +1178,6 @@ class MinesweeperOnline extends Website {
 
     getState() {
         if (
-            document.getElementsByClassName(
-                this.prefix + "_top-area-face-unpressed"
-            ).length
-        ) {
-            return IN_PROGRESS;
-        } else if (
             document.getElementsByClassName(this.prefix + "_top-area-face-lose")
                 .length
         ) {
@@ -1181,7 +1188,7 @@ class MinesweeperOnline extends Website {
         ) {
             return WON;
         }
-        throw new Error("Unknown state");
+        return IN_PROGRESS;
     }
 
     executeSolution(solution) {
@@ -1282,14 +1289,8 @@ class CardGamesIO extends Website {
             return LOST;
         } else if (document.getElementsByClassName("winner").length) {
             return WON;
-        } else if (
-            document
-                .getElementsByClassName("face-top-player")[0]
-                .classList.contains("normal")
-        ) {
-            return IN_PROGRESS;
         }
-        throw new Error("Unknown state");
+        return IN_PROGRESS;
     }
 
     hideProbabilities() {
@@ -1303,8 +1304,213 @@ class CardGamesIO extends Website {
     }
 }
 
+class MinesweeperOne extends Website {
+    constructor() {
+        super();
+        const qp1 = "data:image/gif;base64,R0lGODdh";
+        const qp2 = "APEAAHt7e729vf///wAAACwAAAAA";
+        const qp3 = "EAAQ" + qp2 + "EAAQAAAC";
+        const qp4 = "AAACwAAAAAEAAQAAAC";
+        const qp5 = "DQAXAPEAAAAAAHsAAP8AAAAAACwAAAAADQAXAAAC";
+        this.timeDigits = new Array(10);
+        this.timeDigits[0] =
+            qp1 +
+            qp5 +
+            "OYSPKct8ApqDEcpqHNL6rfxtYQdSZGVSimkEgOtGCPzWT1vHaQKWa6ryoX5CD5C" +
+            "IuYxQDVlJEuQlCgA7";
+        this.timeDigits[1] =
+            qp1 +
+            qp5 +
+            "N4SPCbHN95g0ApoJRHVJ79ZpF3h4VrhhpTgqbMRlyKSCNHWG47N+fL9D5YAxog3" +
+            "0Cj4+nJpCUQAAOw==";
+        this.timeDigits[2] =
+            qp1 +
+            qp5 +
+            "PISPKct8AZoTLzZrIEQuJ0sBmrJUZDhypfetkFSGIHxQMLWtyQbuotFB8IKenM7" +
+            "UE/4wkmVk5hjeZJ9PAQA7";
+        this.timeDigits[3] =
+            qp1 +
+            qp5 +
+            "OYSPKct8AZoTLzZrIEQuJ0sBmrJUZDhypfetkFSGIFzBlLyRXm50Yuv6qXBAlPD" +
+            "ge12CtgjSJvt8CgA7";
+        this.timeDigits[4] =
+            qp1 +
+            qp5 +
+            "PISPCbHNJwybJgJh6bXYQlyBSBd65TmKZMqZX7V1oCfXB1PPt7JJDbtLrDQfzwP" +
+            "oEIp+GtGD8jBBfzxeAQA7";
+        this.timeDigits[5] =
+            qp1 +
+            qp5 +
+            "OYSPKct8ApoLZsUGaLWIapdoF9RlG1meoXklhghJFixzZh0dIsJ5ir1TmYIgFq9" +
+            "nFN4wSByqZnO5CgA7";
+        this.timeDigits[6] =
+            qp1 +
+            qp5 +
+            "OYSPKct8ApoLZsUGaLWIapdoF9RlG1meoXklhghJFixz0YiRNsKBT78bqXhA1NB" +
+            "3jOV0OAeqFnQhCgA7";
+        this.timeDigits[7] =
+            qp1 +
+            qp5 +
+            "OYSPKct8AZoTLzZrIEQuJ0sBmrJUZDhypfet27iFHoy84h2Z5HzqR5f6rXAqFNH" +
+            "QwaVcvFjtFvxICwA7";
+        this.timeDigits[8] =
+            qp1 +
+            "DQAXAPAAAAAAAP8AACwAAAAADQAXAAACNISPGct8AZqDEcpqHNL6rfxtYQdSZGV" +
+            "Siok2kSeFb2yBiY2uqVr24q7zwWyXUWsCi92WhgIAOw==";
+        this.timeDigits[9] =
+            qp1 +
+            qp5 +
+            "OoSPKct8ApqDEcpqHNL6rfxtYQdSZGVSiok2kSeFQNBOB51g8y6qeG+g/Uq3nEa" +
+            "48tV0tJgFFstJDQUAOw==";
+        this.timeNeg =
+            qp1 +
+            qp5 +
+            "MoSPCbHN95g0rzkKba4VcY19ytdd2JgJ6ipo7Ju+rZiQtVae5pah4S2a4H4RzyW" +
+            "nQBQAADs=";
+        this.cellOpenIm = new Array(9);
+        this.cellOpenIm[0] =
+            qp1 +
+            "EAAQAPAAAHt7e729vSwAAAAAEAAQAAACHYSPacHtD550cc5qYZZ4s+6Bm5iRlnl" +
+            "5lNqgHMsUADs=";
+        this.cellOpenIm[1] =
+            qp1 +
+            "EAAQAPEAAAAA/3t7e729vQ" +
+            qp4 +
+            "KYyPacLtH550UQIgq7sYPt5RG5hZV/mdXho2WsOhDBi7Iy1O06vb/VMAADs=";
+        this.cellOpenIm[2] =
+            qp1 +
+            "EAAQAPEAAAB7AHt7e729vQ" +
+            qp4 +
+            "LYyPacLtH550kYGLL7W5t4o5IVNNo1CKJ/p4G7d+MDCRc0fbAp7JtZT66YSNAgA" +
+            "7";
+        this.cellOpenIm[3] =
+            qp1 +
+            "EAAQAPEAAHt7e729vf8AAA" +
+            qp4 +
+            "LISPacHtD550MYiLMWW5i1ZNGhNKY1Ba3eZ4rHiBExeTtFejtyvPUurTBR0FADs" +
+            "=";
+        this.cellOpenIm[4] =
+            qp1 +
+            "EAAQAPEAAAAAe3t7e729vQ" +
+            qp4 +
+            "LoyPacLtH550sYErLqBOZ9xU3zZyjFeGp8aCQtW26oQy4lO/U2rv+b2bBU3DRgE" +
+            "AOw==";
+        this.cellOpenIm[5] =
+            qp1 +
+            "EAAQAPEAAHsAAHt7e729vQ" +
+            qp4 +
+            "KYyPacLtH550UYCLc6u5g81gE2hdI8N5JqmK6OkK1RSnLXuTJ7VDvVMAADs=";
+        this.cellOpenIm[6] =
+            qp1 +
+            "EAAQAPEAAAB7e3t7e729vQ" +
+            qp4 +
+            "LIyPacLtH550kYGLL7U5b4FNTRWKAtmVDJpqKwc4Kiu7J9yOTRvrJvQDBhsFADs" +
+            "=";
+        this.cellOpenIm[7] =
+            qp1 +
+            "EAAQAPEAAAAAAHt7e729vQ" +
+            qp4 +
+            "KoyPacLtH550UYCLc6u5gz0xGBiOTCWZZ3lRk7qmrfvAsWMLaEjzvc8oAAA7";
+        this.cellOpenIm[8] =
+            qp1 +
+            "EAAQAPAAAHt7e729vSwAAAAAEAAQAAACKYSPacHtD550kanV6j3ZYl8F2id+4UZ" +
+            "lV1qGbfeqJ7hacsJOkqvDvVMAADs=";
+        this.bombFlagged =
+            qp1 +
+            "EAAPAPIAAAAAAHt7e729vf8AAP///wAAAAAAAAAAACwAAAAAEAAPAAADOUi63NJ" +
+            "wiaCEvdgGSvIY2VZZ3xdyZAliYpeaLIrBV5sJwDliebzjgKDPFSwWNTKjEfm7MS" +
+            "ebqFSaAAA7";
+        this.bombDisarmed =
+            "data:image/gif;base64,R0lGODlhEAAQAPIEAAAAAHsAAHt7e729vf///wAAA" +
+            "AAAAAAAACH5BAUKAAQALAAAAAAQABAAAAIrlI9pw+0vnnTxhTAruzc33knaAHoW" +
+            "Jk6A+aypA8Qu1cj2PGi3TE+s3wM6CgA7";
+        this.blankCell =
+            qp1 +
+            qp3 +
+            "KpSPGckXYIKcVAIoqn541xt5E5iJVmdyoUmmasmi8Sq2Kchd+q7n/A8oAAA7";
+        this.faceDead =
+            qp1 +
+            "GgAaAPIAAAAAAHt7e729vf//AP///wAAAAAAAAAAACwAAAAAGgAaAAADgBi63M4" +
+            "ikEmrvTNKzK/eQiiOJMlkZVqehAq8b8qW72Db8TiPwO3jugUq1Pv9AKKdoNgrDp" +
+            "rAD4h4c+KqoZ0Tet1KUVbj8dtais9I7VmcFpbDsJqvrRhuj1iyiMmOKs1rT0luJ" +
+            "GFzQXVlPHBIiFM0cTKEKpRZQlIPmQ2Ymp2cnZoJADs=";
+        this.faceSmile =
+            qp1 +
+            "GgAaAPIAAAAAAHt7e729vf//AP///wAAAAAAAAAAACwAAAAAGgAaAAADfxi63M4" +
+            "ikEmrvTNKzK/eQiiOJMlkZVqehAq8b8qW72Db8TiPwO3jugUq1Pv9AKKdoGg8hp" +
+            "TMGi76FLaWPulAu/2AsM0mcscMZ72oshlIvqlxbvQVvD3G5aIyjIvE59ddVQpDR" +
+            "GZ9gl88ajlJVi57Mo4qk4gtD5eYXpibAZqcmAkAOw==";
+        this.faceWin =
+            qp1 +
+            "GgAaAPIAAAAAAHt7AHt7e729vf//AP///wAAAAAAACwAAAAAGgAaAAADhCi63M5" +
+            "DlEmrvTNKzK/eQyiOJMlkZVqehQq8b8qWL2Hb8TiPwO3jugUq1Pv9AKLdoGg8hp" +
+            "RFmLRGQH5AS5y0uq1eUb0wjFsLf1vLYoDKDWjPxKYcuWPKfXQhOos73qxQeF1Ac" +
+            "CJ2TVaFhneJioaHOUl6LlIykyqYT0JXD50NnJ6hoKGeCQA7";
+        this.facePirate =
+            qp1 +
+            "GgAaAPIAAAAAAHt7e729vf//AP///4CAAAAAAAAAACwAAAAAGgAaAAADhxi63M4" +
+            "ikEmrvTNKzK/eQiiOJMlkZVqehAq8b8qW7zAAdjzOI27fv9tugQr5coWgMMQT+A" +
+            "qvpHLZ9MGsRwCT2HL+rrmjkGeFAqRTLTk6TX9A3nZbzTXK5+9iVpndKvRBV2Jae" +
+            "V12d0uFJGJ8Q3+GIjWBhCJNi1cydSqbfhJvD6ANn6Gko6ShCQA7";
+    }
+
+    async confirm() {
+        return true;
+    }
+
+    inject() {
+        const content = document.getElementById("content");
+        content.insertBefore(this.getDiv(), content.firstChild);
+        probabilitiesCheckbox.disabled = true;
+    }
+
+    getCell(x, y) {
+        return document.getElementsByName("cellIm" + x + "_" + y)[0];
+    }
+
+    getValue(elem) {
+        const val = this.cellOpenIm.indexOf(elem.src);
+        if (val !== -1) {
+            return val;
+        }
+        if (elem.src === this.bombDisarmed || elem.src === this.bombFlagged) {
+            return MINE;
+        }
+        return CLOSED;
+    }
+
+    getMineCount() {
+        const hundreds = document.getElementsByName("bomb100s")[0].src;
+        const tens = document.getElementsByName("bomb10s")[0].src;
+        const ones = document.getElementsByName("bomb1s")[0].src;
+        if (hundreds === this.timeNeg) {
+            return (
+                10 * this.timeDigits.indexOf(tens) +
+                this.timeDigits.indexOf(ones)
+            );
+        }
+        return (
+            100 * this.timeDigits.indexOf(hundreds) +
+            10 * this.timeDigits.indexOf(tens) +
+            this.timeDigits.indexOf(ones)
+        );
+    }
+
+    getState() {
+        switch (document.getElementsByName("face")[0].src) {
+            case this.faceDead:
+                return LOST;
+            case this.faceWin:
+            case this.facePirate:
+                return WON;
+            default:
+                return IN_PROGRESS;
+        }
+    }
+}
+
 function getWebsite() {
-    if (window.location.href === "https://minesweeperonline.com/") {
+    if (window.location.href.startsWith("https://minesweeperonline.com/")) {
         return new MinesweeperOnlineCom();
     } else if (
         /^https:\/\/minesweeper\.online\/game\/\d+$/.test(window.location.href)
@@ -1312,6 +1518,8 @@ function getWebsite() {
         return new MinesweeperOnline();
     } else if (window.location.href === "https://cardgames.io/minesweeper/") {
         return new CardGamesIO();
+    } else if (window.location.href === "https://minesweeper.one/") {
+        return new MinesweeperOne();
     }
 }
 
@@ -1334,6 +1542,7 @@ const guessCheckSpan = document.createElement("span");
 const howHardDiv = document.createElement("div");
 const howHardButton = document.createElement("button");
 const howHardSpan = document.createElement("span");
+div.id = "old-sweepy";
 guessLabel.for = "guess";
 guessCheckbox.id = "guess";
 guessCheckbox.type = "checkbox";
